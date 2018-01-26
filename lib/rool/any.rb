6cb695d4e9f7b5a93a250528857be40aed67ea53
Rolf <rdwoolen@gmail.com>
2018-01-26 00:26:11 -0700

    spent hours trying to serialize manually to no avail. ended up using gem oj which was mentioned a lot on stackoverflow, and while i haven't spec tested it yet, it works great in irb. srsly have to figure out how to manually do it with just json lib

--- lib/rool/any.rb
+++ lib/rool/any.rb
@@ -2,7 +2,7 @@ module Rool
   class Any < Container
   	def process(dataset={})
       responses = get_messages(dataset)
-      @messages = responses[:messages] unless responses[:messages].empty?
+      @message = responses[:messages] unless responses[:messages].empty?
       @result = responses[:results].any?
       return @result
   	end
