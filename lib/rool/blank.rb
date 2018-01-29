168ddf1c941d17dc985e36f324e73567d7c92c16
Rolf <rdwoolen@gmail.com>
2018-01-28 20:04:29 -0700

    added rspec tests for all and any

--- lib/rool/blank.rb
+++ lib/rool/blank.rb
@@ -1,10 +1,11 @@
 module Rool
   class Blank < Basic
   	def process(dataset)
-      @result = false unless dataset[@data_key].respond_to?(:empty?) || dataset[@data_key].respond_to?(:nil?)
+      @result = false unless dataset[@data_key].respond_to?(:empty?) || dataset[@data_key].is_a?(NilClass)
       @message = "#{self.class}.process failed because the data type did not respond to :empty? or :nil?" unless @result.nil?
+      return @result unless @result.nil?
       
-      @result = true ? dataset[@data_key].empty? || dataset[@data_key].nil? : @result = false
+      @result = true ? dataset[@data_key].is_a?(NilClass) || dataset[@data_key].empty? : @result = false
       @message = "#{self.class}.process failed because the dataset accessed by #{@data_key} was not blank" if @result == false
       return @result
     end
--- lib/rool/email.rb
+++ lib/rool/email.rb
@@ -1,17 +1,15 @@
 module Rool
   class Email < Basic
-  	def process(dataset={})
-  	  #checks to ensure that key is accessing email
-      @result = true ? @data_key == :email && dataset[@data_key].respond_to?(:to_str) : @result = false
-      @message = "#{self.class}.process failed because the data_key was not :email or data value was not a string" if @result == false
-      
-      #if key is accessing email, ensures that value is a valid email address. re-assigns @result and @message values accordingly
-      if @result == true
-        valid_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-        @result = true ? valid_regex.match?(dataset[@data_key])	: @result = false
-        @message = "#{self.class}.process failed because #{dataset[@data_key]} is not a valid email address." if @result == false 
-      end
-      
+  	def process(dataset)
+      super
+      @result = false unless dataset[@data_key].is_a?(String) 
+      @message = "#{self.class} failed because the data value is not a string" unless @result.nil?
+      return @result unless @result.nil?
+
+      valid_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+      @result = true ? valid_regex.match?(dataset[@data_key])	: @result = false
+      @message = "#{self.class}.process failed because #{dataset[@data_key]} is not a valid email address." if @result == false 
+    
       return @result
     end
   end
