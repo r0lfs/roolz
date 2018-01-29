fa149256ebc2f4befc66ca81bf0db7acb4ee8762
Rolf <rdwoolen@gmail.com>
2018-01-28 17:25:59 -0700

    tweaked my send again so that it's more in line with the rest of the rools. has a suplimental initializer for method and rool type

--- lib/rool/send.rb
+++ lib/rool/send.rb
@@ -1,30 +1,24 @@
 module Rool  
   class Send < Basic
-    attr_accessor :data, :mthd, :rool, :operand, :result, :message
-    def initialize(data, mthd, rool, operand, result = nil, message = nil)
-      if !rool.new.kind_of?(Rool::Basic)
-        raise ArgumentError.new("Expected rule to be kind of Rool::Basic, not #{rool} or #{rool.class}")
-      end
-      if !data.kind_of?(Array)  
-        raise ArgumentError.new("Expected data to be an Array")
-      end
-      if !data.respond_to?(mthd)
-        raise ArgumentError.new("#{mthd} is not a valid method to execute on an Array")
-      end
-      @data = data
+    attr_accessor :mthd, :rool
+    def initialize(data_key, operand, mthd, rool)
+      super(data_key, operand)
       @mthd = mthd 
-      @rool = rool 
-      @operand = operand
+      @rool = rool
+      raise ArgumentError.new("Expected rule to be kind of Rool::Basic, not #{@rool} or #{@rool.class}") unless @rool.new.kind_of?(Rool::Basic)
     end
 
-    #made the 'process' method so that user could change rule, method, data, and operand quickly instead of making new instance
-    def process
-      gnu_data = @data.clone 
-      dataset = {sent: gnu_data.send(@mthd)}
+    def process(dataset)
+      raise ArgumentError.new("Expected accessed dataset to be an Array") unless dataset[@data_key].is_a?(Array)
+      raise ArgumentError.new("#{mthd} is not a valid method to execute on an Array") unless dataset[@data_key].respond_to?(@mthd)
+
+      gnu_data = dataset[@data_key].clone 
+      sent_data = {sent: gnu_data.send(@mthd)}
       gnu_obj = @rool.new(:sent, @operand)
-      @result = gnu_obj.process(dataset)  
-      @message = gnu_obj.message if @result == false
+      @result = gnu_obj.process(sent_data)
+      @message = gnu_obj.message unless @result == true
       return @result 
     end
+    
   end
 end
